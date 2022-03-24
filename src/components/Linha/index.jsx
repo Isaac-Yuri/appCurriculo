8b6f2c277456ec5a3dc77469937450b525f25343
Isaac Yuri <isaacyuri22@gmail.com>
2022-03-24 15:51:14 -0300

    componente Linha adicionado

---
+++ src/components/Linha/index.jsx
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+
+export default function Linha() {
+  return (
+    <View
+      style={{
+        borderTopStyle: "solid",
+        borderWidth: 0.5,
+        borderColor:'rgb(211, 211, 211)',
+        marginTop: 8,
+        width: "100%",
+      }}
+    ></View>
+  );
+}
