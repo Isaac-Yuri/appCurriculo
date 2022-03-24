1571f3d333745c1a8416b5c22284ea41d13f51ec
Isaac Yuri <isaacyuri22@gmail.com>
2022-03-24 10:36:02 -0300

    componente BoxContatos adicionado

---
+++ src/components/BoxContatos/index.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function BoxContatos() {
+  return (
+    <View>
+      <Text>opa</Text>
+    </View>
+  );
+}
--- src/components/Curriculo/index.jsx
+++ src/components/Curriculo/index.jsx
@@ -3,12 +3,14 @@ import { styles } from "./styles";
 
 import Foto from "../Foto";
 import BoxNomeEProfissao from "../BoxNomeEProfissao";
+import BoxContatos from "../BoxContatos";
 
 function Curriculo() {
   return (
     <View style={styles.curriculoContainer}>
       <Foto />
       <BoxNomeEProfissao />
+      <BoxContatos />
     </View>
   );
 }
