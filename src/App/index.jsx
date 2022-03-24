b79559ca086121162cc2cc988abd19858bc9eded
Isaac Yuri <isaacyuri22@gmail.com>
2022-03-24 10:23:24 -0300

    foto adicionada

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,9 +1,11 @@
-import { Text, SafeAreaView } from 'react-native';
+import { SafeAreaView } from "react-native";
+import { styles } from "./style";
+import Curriculo from "../components/Curriculo"; 
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Curriculo />
     </SafeAreaView>
   );
 }
--- src/App/style.js
+++ src/App/style.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native'
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#e7e7e7",
     alignItems: "center",
     justifyContent: "center",
   },
