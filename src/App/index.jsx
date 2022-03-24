bbccda663c8f6b3cd91457b12ca35bc22f228bf7
Isaac Yuri <isaacyuri22@gmail.com>
2022-03-24 08:44:14 -0300

    pasta src criada

---
+++ src/App/index.jsx
@@ -0,0 +1,9 @@
+import { Text, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </SafeAreaView>
+  );
+}
